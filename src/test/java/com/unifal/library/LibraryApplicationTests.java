caff79b11e0badba4ebfcf34c2325ac990e24b6c
João Ragazzo <joao_ragazzo@hotmail.com>
2023-12-10 21:08:03 -0300

    feat: adding first configuration

---
+++ src/test/java/com/unifal/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifal.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
